151b40fadd0d1088e77f046c84e7282b633c6e71
Alexandre Leites <1256013+alexandre-leites@users.noreply.github.com>
2024-05-16 04:20:41 +0200

    Added missing parameters

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,6 +8,7 @@ SLS_HTTP_PORT=${SLS_HTTP_PORT:-8181}
 SLS_SRT_PORT=${SLS_SRT_PORT:-30000}
 SLS_DEFAULT_SID=${SLS_DEFAULT_SID:-live/feed1}
 SLS_SRT_LATENCY=${SLS_SRT_LATENCY:-1000}
+SLS_SRT_TIMEOUT=${SLS_SRT_TIMEOUT:--1}
 
 SLT_SRT_LATENCY=${SLT_SRT_LATENCY:-1000}
 SLT_SRT_LOSSMAXTTL=${SLT_SRT_LOSSMAXTTL:-40}
@@ -16,6 +17,7 @@ SLT_SRT_LOSSMAXTTL=${SLT_SRT_LOSSMAXTTL:-40}
 sed -i "s/http_port [0-9]\+;/http_port ${SLS_HTTP_PORT};/" /etc/sls/sls.conf
 sed -i "s/listen [0-9]\+;/listen ${SLS_SRT_PORT};/" /etc/sls/sls.conf
 sed -i "s/latency [0-9]\+;/latency ${SLS_SRT_LATENCY};/" /etc/sls/sls.conf
+sed -i "s/idle_streams_timeout [0-9]\+;/idle_streams_timeout ${SLS_SRT_TIMEOUT};/" /etc/sls/sls.conf
 sed -i "s/default_sid publish\/[a-zA-Z0-9_]\+;/default_sid publish\/${SLS_DEFAULT_SID};/" /etc/sls/sls.conf
 
 # Start SLS Server
